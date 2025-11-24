1b7df0c4f65a93c0fa4e714d8f907196ba4f40a2
Ray <mindcultivateinfo@gmail.com>
2025-11-24 08:58:48 +0200

    Final code for the URL tracker Version 1.1

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const inputEl = document.querySelector("#input-el")
 const saveBtn = document.querySelector("#save-btn")
 const urlEl = document.querySelector("#url-el")
 const clearBtn = document.querySelector("#clear-btn")
+const grabBtn = document.querySelector("#grab-btn")
+
+grabBtn.addEventListener("click", () => {
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        inputURLs.push(tabs[0].url)
+        
+        render(inputURLs)
+        storeUrlData()
+    })
+})
 
 clearBtn.addEventListener("click", () => {
     localStorage.clear()
@@ -12,114 +22,71 @@ clearBtn.addEventListener("click", () => {
 
 saveBtn.addEventListener("click", () => {
     if (inputEl.value) {
-        console.log("we've got a value")
         inputURLs.push(inputEl.value)
-        console.log(inputURLs)
 
+        render(inputURLs)
+        storeUrlData()
+
+        inputEl.value = ''
+    }else{
+        alert("please add an input value")
+    }
+})
+
+function render(inputURLs) {
         let urls 
         let liEl = document.createElement("li") 
         let togglesDiv = document.createElement('div')
 
         for (let i = 0; i < inputURLs.length; i++) {
-            // console.log(inputURLs[i])
             urls =`
                     <a target="_blank" href="${inputURLs[i]}">
                         ${inputURLs[i]}
                     </a>
                 ` 
-            console.log(urls)
-
-            // let togglesDiv = document.createElement('div')
-            // togglesDiv.setAttribute("class", "toggles")
-
-            // let noteDiv = document.createElement("div")
-            // noteDiv.setAttribute("class", "note")
-            // togglesDiv.appendChild(noteDiv)
-            // let noteImg = new Image()
-            // noteImg.src = "images/edit.png"
-
-            // noteImg.setAttribute("id", "noteImg")
-
-            // noteDiv.appendChild(noteImg)
-            // let noteSpan = document.createElement("span")
-            // noteSpan.setAttribute("class", "note-pop")
-            // noteSpan.innerHTML = "Reminder note"
-            // noteDiv.appendChild(noteSpan)
-
-
-            // let deleteDiv = document.createElement("div")
-            // deleteDiv.setAttribute("class", "delete")
-            // togglesDiv.appendChild(deleteDiv)
-            // let deleteImg = new Image()
-            // deleteImg.src = "images/close.png"
-
-            // deleteImg.setAttribute("id", "deleteImg")
-
-            // deleteDiv.appendChild(deleteImg)
-            // let deleteSpan = document.createElement("span")
-            // deleteSpan.setAttribute("class", "delete-pop")
-            // deleteSpan.innerHTML = "Delete URL"
-            // deleteDiv.appendChild(deleteSpan)
-
-            console.log(togglesDiv)
-            
-            
         }
 
         togglesDiv.setAttribute("class", "toggles")
 
-            let noteDiv = document.createElement("div")
-            noteDiv.setAttribute("class", "note")
-            togglesDiv.appendChild(noteDiv)
-            let noteImg = new Image()
-            noteImg.src = "images/edit.png"
+        let noteDiv = document.createElement("div")
+        noteDiv.setAttribute("class", "note")
+        togglesDiv.appendChild(noteDiv)
+        let noteImg = new Image()
+        noteImg.src = "images/edit.png"
 
-            noteImg.setAttribute("id", "noteImg")
+        noteImg.setAttribute("id", "noteImg")
 
-            noteDiv.appendChild(noteImg)
-            let noteSpan = document.createElement("span")
-            noteSpan.setAttribute("class", "note-pop")
-            noteSpan.innerHTML = "Reminder note"
-            noteDiv.appendChild(noteSpan)
+        noteDiv.appendChild(noteImg)
+        let noteSpan = document.createElement("span")
+        noteSpan.setAttribute("class", "note-pop")
+        noteSpan.innerHTML = "Reminder note"
+        noteDiv.appendChild(noteSpan)
 
 
-            let deleteDiv = document.createElement("div")
-            deleteDiv.setAttribute("class", "delete")
-            togglesDiv.appendChild(deleteDiv)
-            let deleteImg = new Image()
-            deleteImg.src = "images/close.png"
+        let deleteDiv = document.createElement("div")
+        deleteDiv.setAttribute("class", "delete")
+        togglesDiv.appendChild(deleteDiv)
+        let deleteImg = new Image()
+        deleteImg.src = "images/close.png"
 
-            deleteImg.setAttribute("id", "deleteImg")
+        deleteImg.setAttribute("id", "deleteImg")
 
-            deleteDiv.appendChild(deleteImg)
-            let deleteSpan = document.createElement("span")
-            deleteSpan.setAttribute("class", "delete-pop")
-            deleteSpan.innerHTML = "Delete URL"
-            deleteDiv.appendChild(deleteSpan)
+        deleteDiv.appendChild(deleteImg)
+        let deleteSpan = document.createElement("span")
+        deleteSpan.setAttribute("class", "delete-pop")
+        deleteSpan.innerHTML = "Delete URL"
+        deleteDiv.appendChild(deleteSpan)
 
-        console.log(urls)
         liEl.innerHTML = urls
         liEl.appendChild(togglesDiv)
         urlEl.append(liEl)
-
-        storeUrlData()
-
-        console.log(inputURLs)
-        inputEl.value = ''
-
-    }else{
-        console.log("please add an input value")
-    }
-})
+}
 
 urlEl.addEventListener("click", (e) => {
-    console.log(e)
     if (e.target.id === "noteImg") {
-        alert("Yea!! Were taking notes!")
         let noteEl = document.createElement("p")
         noteEl.setAttribute("contenteditable", true)
         noteEl.setAttribute("id", "note-el")
-        console.log(noteEl)
         
         noteEl.style.visibility = "visible"
         noteEl.style.opacity = 1
@@ -133,18 +100,14 @@ urlEl.addEventListener("click", (e) => {
 
         storeUrlData()
     }else if (e.target.tagName === "P") {
-        console.log("Yahooo!")
         const notes = document.querySelectorAll("#note-el")
-        console.log(notes)
 
         notes.forEach(note => {
             note.onkeyup = function() {
-                console.log("storing!")
                 storeUrlData()
             }
         })
     }
-
 })
 
 
@@ -154,7 +117,6 @@ function storeUrlData() {
 
 function showStoredURLs() {
     let storedURLs = JSON.parse(localStorage.getItem("URLs") )
-    console.log(storedURLs)
     
     urlEl.innerHTML = storedURLs
 }
